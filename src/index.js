8137736de95de3dd0c84ae274ff5895cc636e752
namtieu <hoainam.tieu@gmail.com>
2026-01-27 03:20:51 +0700

    fix app listen port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+const port = 3000;
+
 app.get("/", (req, res) => {
   res.send("CI/CD on Windows is working 🚀");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
